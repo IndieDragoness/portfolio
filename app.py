7c966d11828fbf86845fd0ca605cb2962b56b8f2
Sage <sagesingularity@gmail.com>
2021-03-30 12:19:22 -0500

    Partition Key Update
    It turns out shards/partitions are enforced, and you have to manually add partition keys into the data itself. The format for this is:
    
    ```
    {'id': email, 'name': name, 'subject': subject, 'message': message, 'portfolio_section': "contact_form"}
    ```
    
    Where portfolio_section in this case is considered the partition key. This key value is necessary when doing point read/writes, as opposed to doing general multi-partition SQL queries which are much more expensive.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
+from azure.core.exceptions import ResourceExistsError
+import logging
 import json
 
 # Microsoft Azure Cosmos DB Initialization
@@ -11,12 +13,15 @@ client = CosmosClient(endpoint, key)
 database_name = 'Portfolio_DB'
 database = client.get_database_client(database_name)
 
-container_name = 'Contact_Form'
+container_name = 'Portfolio_Container'
 container = database.get_container_client(container_name)
 
 # Flask
 # Static is where all of our static files are stored
 app = Flask(__name__, static_url_path='/static')
+
+# Setup Flask Logging to record events at Runtime
+logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
@@ -25,21 +30,46 @@ def main():
 # Section correlates to the Contact Form in index.htm
 @app.route('/contact_form_action', methods=['POST'])
 def contact_form_action():
+    app.logger.info('Contact Form Submission Detected!')
+
     # Retrieve data from HTML inputs
     name = request.form['Name']
     email = request.form['Email']
     subject = request.form['Subject']
     message = request.form['Message']
 
-    # Get current Contact_Form container in Microsoft Azure Cosmos DB
-    items = list(container.query_items(
-    query="SELECT * FROM c WHERE c.name IN ('Sage')",
-    enable_cross_partition_query=True
-    ))
+    # Log data entered
+    app.logger.info('Name: ' + name)
+    app.logger.info('Email: ' + email)
+    app.logger.info('Subject: ' + subject)
+    app.logger.info('Message: ' + message)
 
     # Post data to Contact_Form container in Microsoft Azure Cosmos DB
-    new_entry = {'id': str(len(items) + 1), 'name': name, 'email': email, 'subject': subject, 'message': message}
-    container.create_item(new_entry)
+    try:
+        app.logger.info('Checking if email is already present in database...')
+        # Portfolio Section is the Partition Key for the Portfolio Container (used for point reads and writes)
+        new_entry = {'id': email, 'name': name, 'subject': subject, 'message': message, 'portfolio_section': "contact_form"}
+        container.create_item(new_entry)
+        app.logger.info('Email not present! Created new database entry successfully: ' + email)
+    except ResourceExistsError:
+        app.logger.info('Email already present in database. Adding new message to existing entry: ' + email)
+
+        # Get the current entry in the database for this email and add this new message
+        properties = container.read()
+        app.logger.info(properties)
+        items = container.read_all_items()
+        app.logger.info(items)
+
+        item = container.read_item(email, partition_key="contact_form")
+        app.logger.info('Acquired item.\n{}'.format(item))
+
+        message_count = 0
+        for entry in item.keys():
+            if 'message' in entry:
+                message_count += 1
+        app.logger.info('This is the {} message for this id.'.format(message_count))
+        item["message{}".format(message_count)] = message
+        updated_item = container.upsert_item(item)
 
     # Scroll the page down to the Contact Form, after Submit is pressed, and say 'Thank you!'
     return render_template("/index.htm", submit_button_pressed="contact")
